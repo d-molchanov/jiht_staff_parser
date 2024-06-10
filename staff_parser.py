cdf517ce1b9dd67789ecfaa81c5b732078b962ab
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2024-06-10 13:55:03 +0300

    Method `_load_dotenv` for loading login and password from .env-file was added.

--- staff_parser.py
+++ staff_parser.py
@@ -1,21 +1,28 @@
 import os
+from typing import List
 import logging
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
+
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:\t%(message)s',
-    level=logging.INFO
+    level=logging.DEBUG
 )
+
+
 
 class StaffParser:
 
-    def __init__(self):
-        load_dotenv(os.path.abspath('.env'))
+    def __init__(self) -> None:
+        self._load_dotenv('./.env')
+
+    def _load_dotenv(self, path_: str) -> None:
+        load_dotenv(os.path.abspath(path_))
+        logging.debug('dotenv values: %s', dotenv_values())
 
 
 def test():
     staff_parser = StaffParser()
-    logging.info(os.environ.get('JIHT_LOGIN'))
 
 
 if __name__ == '__main__':
